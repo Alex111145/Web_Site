55e8b4072a75a9c2e9d1fe98ec265225fefd9948
Ale <159631777+Alex111145@users.noreply.github.com>
2026-03-09 10:18:20 +0100

    dev 3.4

--- src/components/ProfileCard/ProfileCard.jsx
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -84,8 +84,8 @@ const ProfileCardComponent = ({
         "--pointer-from-center": `${clamp(Math.hypot(percentY - 50, percentX - 50) / 50, 0, 1)}`,
         "--pointer-from-top": `${percentY / 100}`,
         "--pointer-from-left": `${percentX / 100}`,
-        "--rotate-x": `${round(-(centerX / 4))}deg`,
-        "--rotate-y": `${round(centerY / 4)}deg`,
+"--rotate-x": `${round(-(centerY / 4))}deg`, // Movimento Verticale -> Rotazione X
+  "--rotate-y": `${round(centerX / 4)}deg`,
       };
 
       Object.entries(properties).forEach(([property, value]) => {
