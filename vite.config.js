5b0cc70e0aed0b5d7e7b4ede2885d4d7e09093da
Ale <159631777+Alex111145@users.noreply.github.com>
2026-03-09 10:29:06 +0100

    dev 3.6

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/Web_Site/",
+  base: "/Web_Site/", // Questo deve corrispondere al nome del repository
   plugins: [react(), tailwindcss()],
 })
